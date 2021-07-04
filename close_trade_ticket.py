9c996467b5e669975309c063cf6e9138e9cf0664
Neel Patel <neel@hotmail.com>
2021-07-04 22:51:03 +0100

    open trade ticket

---
+++ close_trade_ticket.py
@@ -0,0 +1,9 @@
+_zmq._DWX_MTX_CLOSE_TRADE_BY_TICKET_(85051856)
+
+# MetaTrader response (JSON):
+{'_action': 'CLOSE', 
+'_ticket': 85051856, 
+'_close_price': 1.14378, 
+'_close_lots': 0.04, 
+'_response': 'CLOSE_MARKET', 
+'_response_value': 'SUCCESS'}
