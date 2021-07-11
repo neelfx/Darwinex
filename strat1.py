0e768045cdba430c92a59779d233e255dd35d07f
Neel Patel <neel@hotmail.com>
2021-07-11 19:29:11 +0100

    strat1

--- strat1.py
+++ strat1.py
@@ -119,3 +119,16 @@ _zmq._DWX_MTX_CLOSE_ALL_TRADES_()
  '_response_value': 'SUCCESS'}
 
  ###########################################################
+
+ _zmq._DWX_MTX_CLOSE_TRADE_BY_TICKET_(85051856)
+
+# MetaTrader response (JSON):
+{'_action': 'CLOSE', 
+'_ticket': 85051856, 
+'_close_price': 1.14378, 
+'_close_lots': 0.04, 
+'_response': 'CLOSE_MARKET', 
+'_response_value': 'SUCCESS'}
+
+#############################################################
+
