0e9d9496704ed1bc80832787baa43460d76898e0
Neel Patel <neel@hotmail.com>
2021-07-11 19:30:04 +0100

    strat1

--- strat1.py
+++ strat1.py
@@ -132,3 +132,19 @@ _zmq._DWX_MTX_CLOSE_ALL_TRADES_()
 
 #############################################################
 
+_zmq._DWX_MTX_GET_ALL_OPEN_TRADES_()
+
+# MetaTrader response (JSON):
+
+{'_action': 'OPEN_TRADES', 
+'_trades': {
+    85051741: {'_magic': 123456, 
+                '_symbol': 'EURUSD', 
+                '_lots': 0.05, 
+                '_type': 0, 
+                '_open_price': 1.14414, 
+                '_pnl': -0.45}
+    }
+}
+
+##################################################################
